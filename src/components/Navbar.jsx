c94624a9c4507aa754c73f7408d8b9bff3985665
neha12N <nehakumari29122003@gmail.com>
2025-11-01 15:38:18 +0530

    starting project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-amber-100 flex justify-between">
+        <h1></h1>
+
+    </nav>
+  )
+}
+
+export default Navbar
